6b7e8e7acf34dabfffdb7578cc7a00d016a9f703
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-07-22 16:44:44 +0200

    Pokemonsdedicace

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,10 +13,12 @@ use Symfony\Component\Routing\Attribute\Route;
 // on etend la Classe extends, AbstractController est dans Vendor pret a l emploi, pour recuperer tout ce qu il y a dans le Parent sauf ce qui sera mis en private
 class IndexController extends AbstractController //on etend la classe Abstratccontroller qui permet d utiliser les fonctions utilitaires pour que symfony fasse le require de ces classes
 {
-    #[Route('/', name: 'app_IndexController')] // je cree une nouvelle route et une page ('/') qui affiche // # annotation equivalent de if comme dans index.php avant, permet de creer une nouvelle route cad une nouvelle page sur notre appli quand l url est appelee cela execute automatiquement la methode definit sous la route
+    #[Route('/', name: 'app_IndexController')] // je cree une nouvelle route et une page ('/') qui affiche // # annotation equivalent de if comme dans index.php avant, permet de creer une nouvelle route cad une nouvelle page sur notre appli quand l url est appelée cela execute automatiquement la methode definit sous la route
     public function index () {  
 
-        var_dump('value:"hello World"');die; // je fais un var_dump pour recuperer un affichage "hello World
+        //var_dump('value:"hello World"');die; // je fais un var_dump pour recuperer un affichage "hello World"
+
+        return $this->render('Page/indexView.html.twig'); // on peut directement render au lieu de sur deux lignes comme auparavant, cette methode est dans l AbstractController”
     } 
 
 }
